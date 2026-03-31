de04bcd0a4e302bde26224a3d0450f4520875b24
Rahul <rahulravilla18@gmail.com>
2026-03-31 22:07:00 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-import streamlit as st  
-import pickle 
+import streamlit as st
+import pickle
 
 movies = pickle.load(open("movie_list.pkl",'rb'))
 similarity = pickle.load(open("similarity.pkl",'rb'))
+
 movies_list = movies['title'].values
 
+st.header("Movie Recommendation System")
 
-st.header("Movie Recomendation System")
-st.selectbox("select movie from dropdown ", movies_list)
+selected_movie = st.selectbox("Select movie from dropdown", movies_list)
 
-def recommand(movies):
-    index = movies[movies['title']== movies].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse = True, key = lambda vector:vector[1])
-    recommend_movie=[]
-    for i in distance[0:5]:
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])),
+                      reverse=True,
+                      key=lambda vector: vector[1])
+    
+    recommend_movie = []
+    for i in distance[1:6]:   # skip itself
         recommend_movie.append(movies.iloc[i[0]].title)
-    return recommend_movie       
+        
+    return recommend_movie
 
 
-if st.button("Show recommend"):
-    movie_name = recommend(selectvalue)
+if st.button("Show Recommend"):
+    movie_name = recommend(selected_movie)
+
     col1, col2, col3, col4, col5 = st.columns(5)
+
     with col1:
         st.text(movie_name[0])
     with col2:
@@ -31,6 +39,3 @@ if st.button("Show recommend"):
         st.text(movie_name[3])
     with col5:
         st.text(movie_name[4])
-        
-
-
